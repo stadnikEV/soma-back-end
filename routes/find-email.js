d2f1968e742b529d37f08fa2d3b6fd093e21e707
stadnikEV <stadnik24fps@gmail.com>
2019-02-12 18:38:49 +0200

    fix: find email

--- routes/find-email.js
+++ routes/find-email.js
@@ -1,14 +1,13 @@
-// const getEmail = require("../libs/get-email");
 
 module.exports = (req, res, next) => {
   const Company = require('../models/company');
   const email = req.body.email;
-
-  Company.find({ email }, ['-_id', 'email' ])
+  console.log(email);
+  Company.find({ email: { $in: email } }, ['-_id', 'email' ])
     .then((documents) => {
       res.json(documents);
     })
     .catch((e) => {
       next(e);
     });
-};
+}
